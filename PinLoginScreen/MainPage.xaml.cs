1fa2e3079e8ddddd97ceba338859789b431bfa00
ThinhTran <thinhtran@outlook.com.au>
2019-06-21 12:15:30 +0800

    add number layout

--- PinLoginScreen/MainPage.xaml.cs
+++ PinLoginScreen/MainPage.xaml.cs
@@ -13,9 +13,44 @@ namespace PinLoginScreen
     [DesignTimeVisible(false)]
     public partial class MainPage : ContentPage
     {
+        int currentState = 1;
+        int firstNumber, secondNumber;
+
         public MainPage()
         {
             InitializeComponent();
         }
+
+        void OnSelectNumber(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            string pressed = button.Text;
+
+            if (this.resultText.Text == "0" || currentState < 0)
+            {
+                this.resultText.Text = "";
+                if (currentState < 0)
+                    currentState *= -1;
+            }
+
+            this.resultText.Text += pressed;
+
+            int number;
+            if (int.TryParse(this.resultText.Text,  out  number))
+            {
+                this.resultText.Text = number.ToString("N0");
+                if (currentState == 1)
+                {
+                    firstNumber = number;
+                }
+                else
+                {
+                    secondNumber = number;
+                }
+            }
+        }
+
+
+
     }
 }
